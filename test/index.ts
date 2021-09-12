731c61594e15c0b3e52d36ea6a957b1f4cc901a7
tl-maisie-sadler <maisie.sadler@truelayer.com>
2021-09-12 13:51:42 +0100

    initial

---
+++ test/index.ts
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import 'mocha';
+
+describe('Routing Tests', function () {
+    describe('Expected route returns 200', function () {
+
+        it('GET /{accountId}', async () => {
+            expect(200).to.equal(401);
+        });
+    });
+});
